6c9c4a70f3f72225a1033648aa3ef70e0bbfd4af
terrylinooo <terrylin.developer@gmail.com>
2019-05-28 15:31:33 +0800

    update website

---
+++ _book/build_release.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+dir=$(dirname $0)
+
+echo ${dir};
+
+# Move files from git to svn repository.
+gitbook build
+
+
+cp -a ${dir}/_book ${dir}/github_page
+
+cd github_page
+git add .
+git commit -m "update website"
+git push origin master
+cd ../
